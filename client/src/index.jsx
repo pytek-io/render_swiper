fdfde6ba4e888e9d4e202a5f6bf4eeb1b4a6edea
fdv1 <francois@pytek.io>
2024-07-28 11:46:15 +0100

    automatic generation

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,13 @@
 /* AUTO GENERATED FILE - DO NOT EDIT! */
-
 import { registerComponent, signal_script_loaded } from "render";
-import { lazy } from "react";
-import 'swiper/css';
-import 'swiper/css/navigation';
 
-registerComponent('Swiper', '', lazy(() => import("./fragments/swiper/react/swiper.tsx")), 'swiper/react', true)
-registerComponent('SwiperSlide', '', lazy(() => import("./fragments/swiper/react/swiperslide.tsx")), 'swiper/react', true)
+import "swiper/css";
+import "swiper/css/navigation";
+
+import Swiper from "./fragments/swiper/react/swiper.tsx";
+import SwiperSlide from "./fragments/swiper/react/swiperslide.tsx";
+
+
+registerComponent('Swiper', '', Swiper, 'swiper/react', false, true)
+registerComponent('SwiperSlide', '', SwiperSlide, 'swiper/react', false, false)
 signal_script_loaded("swiper/react");

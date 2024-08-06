c529001fa02188cbb769ec41a5fc60970aaa30ce
fdv1 <francois@pytek.io>
2024-08-06 15:08:58 +0200

    remove fragments folder

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,10 @@
 /* AUTO GENERATED FILE - DO NOT EDIT! */
-import { registerComponent, signal_script_loaded } from "render";
 
+import { registerComponent, signal_script_loaded } from "render";
 import "swiper/css";
 import "swiper/css/navigation";
-
-import Swiper from "./fragments/swiper/react/swiper.tsx";
-import SwiperSlide from "./fragments/swiper/react/swiperslide.tsx";
-
+import Swiper from "./swiper/react/swiper.tsx";
+import SwiperSlide from "./swiper/react/swiperslide.tsx";
 
 registerComponent('Swiper', '', Swiper, 'swiper/react', false, true)
 registerComponent('SwiperSlide', '', SwiperSlide, 'swiper/react', false, false)
